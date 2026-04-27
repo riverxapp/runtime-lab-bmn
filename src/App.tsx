afd51e08ac632e63b1eb0dd4fd6be3861ec4380b
Chirag Dodiya <hi@chirag.co>
2026-04-27 19:12:26 +0530

    chore: deterministic patch step 1/1 - src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
-
-import { AppRoutes } from "./routes";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "../pages/home";
+import { NotFoundPage } from "../pages/NotFoundPage";
 
 export function App() {
-  return <AppRoutes />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
+  );
 }
